e52c5b9c03627ef379296d8d4bd5becfff2f475b
Chen Shuo <546537825@qq.com>
2016-10-12 11:57:39 +0800

    Test react component.

---
+++ src/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import App from './components/App';
+
+
+
+function run()
+{
+    render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+
+run();
