ded6e4763b4d92994848075c93f542d0fcd492cb
Chen Shuo <546537825@qq.com>
2016-10-17 01:35:41 +0800

    Add search bar event.

--- src/home/components/App.js
+++ src/home/components/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import CourseContent from "./CourseContent";
 import Header from "./Header";
 import SearchBar from "./SearchBar";
+import ServiceClient from "../service/ServiceClient";
 
 export default class App extends Component {
 
     constructor (props) {
         super(props);
         console.log("Courseplus home is running......");
+
+        this.handleSearch = this.handleSearch.bind(this);
+
+        this.loadInitialData();
     }
 
     static defaultProps = {
@@ -20,19 +25,50 @@ export default class App extends Component {
     }
 
     state = {
-
+        selectedSchool: "",
+        majors: [],
+        selectedMajor: "",
+        content: []
     }
 
     render()
     {
+        const state = this.state;
+
         return (
             <div className="cp-home-app">
                 <header><Header /></header>
                 <main>
-                    <div className="tool-bar"><SearchBar /></div>
-                    <div className="content"><CourseContent /></div>
+                    <div className="tool-bar">
+                        <SearchBar
+                            school={state.selectedSchool}
+                            majors={state.majors}
+                            selectedMajor={state.selectedMajor}
+                        /></div>
+                    <div className="content"><CourseContent courses={state.content}/></div>
                 </main>
             </div>
         );
+    }
+
+    loadInitialData()
+    {
+        ServiceClient.getInstance().getHomeData().then(data => {
+            const school = data.school;
+            const majors = data.majors;
+            ServiceClient.getInstance().getCoursesByMajor(majors[0]).then(courses => {
+                this.setState({
+                    selectedSchool :school,
+                    majors,
+                    selectedMajor: majors[0],
+                    content: courses
+                });
+            });
+        });
+    }
+
+    handleSearch(key)
+    {
+
     }
 }
--- src/home/components/SearchBar.js
+++ src/home/components/SearchBar.js
@@ -7,7 +7,9 @@ export default class SearchBar extends Component {
     }
 
     static defaultProps = {
-
+        school: "",
+        majors: [],
+        selectedMajor: ""
     }
 
     static propTypes = {
@@ -25,24 +27,32 @@ export default class SearchBar extends Component {
 
     render()
     {
+        const { school, majors, selectedMajor} = this.props;
+
         return (
             <div className="cp-home-search">
                 <span className="school-tag">学校：</span>
                 <div className="school-dropdown dropdown">
                     <button className="btn-school btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-                        南京大学<span className="caret"></span>
+                        {school}<span className="caret"></span>
                     </button>
                     <ul className="dropdown-menu">
-                        <li><a>南京大学</a></li>
+                        <li><a>{school}</a></li>
                     </ul>
                 </div>
                 <span className="major-tag">专业:</span>
                 <div className="major-dropdown dropdown">
                     <button className="btn-major btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-                        软件工程<span className="caret"></span>
+                        {selectedMajor.name}<span className="caret"></span>
                     </button>
                     <ul className="dropdown-menu">
-                        <li><a>软件工程</a></li>
+                        {majors.map(item => {
+                            return (<li
+                                        ref={item.id}
+                                        key={item.id}
+                                        onClick={() => this.onSelectionChange(item)}
+                                    ><a>{item.name}</a></li>)
+                        })}
                     </ul>
                 </div>
                 <div className="empty"></div>
@@ -53,5 +63,10 @@ export default class SearchBar extends Component {
 
             </div>
         );
+    }
+
+    onSelectionChange(major)
+    {
+        console.log(major);
     }
 }
