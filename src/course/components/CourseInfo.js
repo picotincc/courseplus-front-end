b94dba118c85f3840119fefb7449c8d20e3d5234
Chen Shuo <546537825@qq.com>
2016-10-18 01:46:59 +0800

    Add course info css.

--- src/course/components/CourseInfo.js
+++ src/course/components/CourseInfo.js
@@ -31,9 +31,26 @@ export default class CourseInfo extends Component {
                     <img src='http://i1.piimg.com/573251/65de634ba7719cf2.jpg' />
                 </div>
                 <div className="intro">
-                    <div className="title">数据结构</div>
+                    <div className="title">
+                        <span className="name">数据结构</span>
+                        <span className="knowledge"></span>
+                        <span className="knowledge-count">20</span>
+                        <span className="resource"></span>
+                        <span className="resource-count">18</span>
+                    </div>
+                    <div className="label-group">
+                        <div className="label school">
+                            <span>南京大学</span>
+                        </div>
+                        <div className="label major">
+                            <span>软件工程</span>
+                        </div>
+                    </div>
                     <div className="para">
                         数据结构数数据结构数据结构数据结构数数据结构数据结构
+                    </div>
+                    <div className="bottom">
+                        <a>查看全部</a>
                     </div>
                 </div>
             </div>
