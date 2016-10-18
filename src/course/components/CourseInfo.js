3f44f398a625ec6e923d242a57db06ca2917946a
Chen Shuo <546537825@qq.com>
2016-10-18 16:52:55 +0800

    Finish course info.

--- src/course/components/CourseInfo.js
+++ src/course/components/CourseInfo.js
@@ -4,6 +4,8 @@ export default class CourseInfo extends Component {
 
     constructor (props) {
         super(props);
+
+        this.showAllPara = this.showAllPara.bind(this);
     }
 
     static defaultProps = {
@@ -28,11 +30,11 @@ export default class CourseInfo extends Component {
         return (
             <div className="cp-course-info">
                 <div className="image">
-                    <img src='http://i1.piimg.com/573251/65de634ba7719cf2.jpg' />
+                    <img src='https://img3.doubanio.com/lpic/s1106991.jpg' />
                 </div>
                 <div className="intro">
                     <div className="title">
-                        <span className="name">数据结构</span>
+                        <span className="name">2天精通数据结构</span>
                         <span className="knowledge"></span>
                         <span className="knowledge-count">20</span>
                         <span className="resource"></span>
@@ -46,14 +48,44 @@ export default class CourseInfo extends Component {
                             <span>软件工程</span>
                         </div>
                     </div>
-                    <div className="para">
-                        数据结构数数据结构数据结构数据结构数数据结构数据结构
+                    <div className="para" ref="para">
+                        <p>&nbsp;数据结构数数据结构数据结构数据结构数数据结构数据结构数据结
+                        构数数据结构数据结构数据结构数数据结构数据结构数据结构数数据结构数
+                        据结构数据结构数数据结构数据结构数据结构数数据结构数据结构
+                        数据结构数数据结构数据结构数据结构数数据结构数据结构数据结构
+                        数数据结构数据结构数据结构数数据结构数据结构数据结构数数据
+                        结构数据结构数据结构数数据结构数据结构数据结构数数据结构数据结
+                        构数据结构数数据结构数据结构数据结构数数据结构数据结构数据结
+                        构数数据结构数据结构数据结构数数据结构数据结构数据结构数数据
+                        结构数据结构数据结构数数据结构数据结构数据结构数数据结构
+                        数据结构数据结构数数据结构数据结构数据结构数数据结构数据结
+                        </p>
                     </div>
                     <div className="bottom">
-                        <a>查看全部</a>
+                        <a ref="showAll" onClick={this.showAllPara}><u>查看全部</u></a>
                     </div>
                 </div>
             </div>
         );
+    }
+
+    showAllPara()
+    {
+        const $btnShow = $(this.refs.showAll);
+        const para = this.refs.para;
+
+        if ($btnShow.text() === "查看全部") {
+            para.style.height = "auto";
+            $btnShow.empty();
+            let $addDom = $("<u>收起全文</u>");
+            $btnShow.append($addDom);
+        }
+        else
+        {
+            para.style.height = "105px";
+            $btnShow.empty();
+            let $addDom = $("<u>查看全部</u>");
+            $btnShow.append($addDom);
+        }
     }
 }
