aeb4ac5621ebcd6925a9512d19d4b188212ef14f
Chen Shuo <546537825@qq.com>
2016-10-20 01:24:35 +0800

    Fix css bug.

--- src/course/components/Course.js
+++ src/course/components/Course.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Contributor from "./Contributor";
 import CourseInfo from "./CourseInfo";
+import Forum from "./Forum";
+import Resources from "./Resources";
 
 export default class Course extends Component {
 
@@ -37,8 +39,10 @@ export default class Course extends Component {
                     <Contributor />
                 </div>
                 <div className="discuss-area">
+                    <Forum />
                 </div>
                 <div className="related-resources">
+                    <Resources />
                 </div>
             </div>
         );
