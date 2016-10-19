ffa7158e5f8bc7373e001d3c69f5f9fc5c7a3928
Chen Shuo <546537825@qq.com>
2016-10-20 01:24:49 +0800

    Add Forum component.

---
+++ src/course/components/Forum.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class Forum extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    static defaultProps = {
+
+    }
+
+    static propTypes = {
+
+    }
+
+    state = {
+        isLogin: true
+    }
+
+    render()
+    {
+        return (
+            <div className="cp-course-forum">
+                <div className="title">讨论区</div>
+                <div className="comment-input">
+                    <textarea className="forum-textarea" placeholder="说点什么吧"/>
+                </div>
+                <div className="bar">
+                    <div className="btn-publish">
+                        <span>发布</span>
+                    </div>
+                </div>
+                <ul className="comment-list">
+                </ul>
+            </div>
+        );
+    }
+}
