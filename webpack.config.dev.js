52cd9790e02cca0ab37b7d27452d57d6c547ac48
Chen Shuo <546537825@qq.com>
2016-11-07 16:39:23 +0800

    Add babel-polyfill.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = {
     context: path.resolve("./src"),
 
     entry: {
-        vendor: [ "jquery", "./base/lib/bootstrap.min.js", "sweetalert", "pingpp-js", "pubsub-js" ],
+        vendor: [ "babel-polyfill", "jquery", "./base/lib/bootstrap.min.js", "sweetalert", "pingpp-js", "pubsub-js" ],
         user: [ "./user/index.js", "./user/resource/index.less" ],
         home: [ "./home/index.js", "./home/resource/index.less" ],
         course: [ "./course/index.js", "./course/resource/index.less" ],
