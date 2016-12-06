9bbca73416039111c55321d3254d0e05f6b60c78
Chen Shuo <546537825@qq.com>
2016-12-06 14:12:33 +0800

    Refactor analysis code.

--- src/index/index.js
+++ src/index/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import AnalysisUtil from "../base/util/AnlaysisUtil";
 import App from './containers/App';
 
 
 
 function run()
 {
+    AnalysisUtil.createBdAnalysisEntry();
     render(
         <App />,
         document.getElementById("root")
