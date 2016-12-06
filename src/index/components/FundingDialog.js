08a2586afd8e87597fac7085dd37e818abc45bff
Chen Shuo <546537825@qq.com>
2016-12-06 11:05:24 +0800

    Finish funding static page.

--- src/index/components/FundingDialog.js
+++ src/index/components/FundingDialog.js
@@ -25,6 +25,37 @@ export default class FundingDialog extends Component {
 
     render()
     {
+        return (
+            <div className="cp-index-funding">
 
+                <div className="content">
+                    <div className="top-bar">
+                        <span onClick={this.props.onFundingHide} className="icon iconfont icon-close"></span>
+                    </div>
+                    <div className="title-section">
+                        <img src="/imgs/funding-title.png" />
+                    </div>
+                    <div className="line">
+                    </div>
+                    <div className="text">
+                        <div className="first">
+                            米盒百万资料众筹&nbsp;正在进行中
+                        </div>
+                        <div className="second">
+                            点击立即参加&nbsp;共享百万资料
+                        </div>
+                    </div>
+                    <div className="join">
+                        <div className="join-btn">
+                            我要参加
+                        </div>
+                        <img src="/imgs/pointer.png" />
+                    </div>
+                    <div className="buttom-bar">
+                        *本次活动解释权归一可米互联网科技公司所有
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
